e930c067e141cd561f46deccf452612c1dd3ac7b
kbhasin31 <meetkartik.31@gmail.com>
2024-08-17 12:44:09 +0530

    adding pages

--- application/CodeFury-WFS1-TEAM2-UI/assets/js/script.js
+++ application/CodeFury-WFS1-TEAM2-UI/assets/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
+if(bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
+};
+
+if(close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  })
+};
 // modal variables
 const modal = document.querySelector('[data-modal]');
 const modalCloseBtn = document.querySelector('[data-modal-close]');
